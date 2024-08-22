ed77c98fd7a171a568e8fc06f9f05ae89c0fd44c
Hyde Song <hyde@Hydes-MacBook-Pro.local>
2024-08-22 18:15:46 +0800

    tabs

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,14 @@
 import HeaderBox from '@/components/HeaderBox';
 import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import Tabs from '@/components/Tabs'
+
+//tabData mock:
+const tabData = [
+    { id: 'tab1_unique_id', title: 'Tab 1', content: 'Content for Tab 1' },
+    { id: 'tab2_unique_id', title: 'Tab 2', content: 'Content for Tab 2' },
+    { id: 'tab3_unique_id', title: 'Tab 3', content: 'Content for Tab 3' },
+]
 
 const Home = () => {
     const loggedIn = { firstName: 'Hyde', lastName: 'Song', email: 'hyde.song@icloud.com' }
@@ -61,6 +69,18 @@ const Home = () => {
                 </header>
 
                 recent transactions
+                {/* <div className="tabs-wrapper">
+                    <Tabs>
+                        <Tabs.Titles items={tabData.map(({ id, title }) => ({ id, title }))} />
+                        <Tabs.Contents
+                            items={tabData.map(({ id, content }) => ({
+                                id,
+                                content: <p>{content}</p>,
+                            }))}
+                        />
+                    </Tabs>
+                </div> */}
+
             </div>
 
             <RightSidebar
